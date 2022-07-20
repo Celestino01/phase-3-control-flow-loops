0bfa446e1a4f008b58ccaaf202d48597fbdb4bb5
celestino <celestino.martinez01@gmail.com>
2022-07-20 14:22:43 -0400

    commit tree 616186f854413cb72ea3c8f8fb8ad79b640712c7

--- looping.rb
+++ looping.rb
@@ -1,18 +1,28 @@
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter 
+    counter -= 1
+    if counter < 1
+      puts "Happy New Year!"
+    end
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
+  (1..num).each do|i|
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
   else
-    num
+    puts i
   end
+end
 end
 
 def fizzbuzz_printer
